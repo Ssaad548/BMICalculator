7ae76732ae1af81130221d237f34cd17f7f4a91d
Ssaad548 <syedsaad547@gmail.com>
2024-07-05 23:57:55 +0800

    Frontend Files

---
+++ BMICalculator/BMIRecord.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BMICalculator
+{
+    public class BMIRecord
+    {
+        public string DateRecorded {  get; set; }
+        public double Weight { get; set;}
+        public double BMIResult { get; set; }
+        public string BMIStatus { get; set; }
+    }
+}
---
+++ BMICalculator/FirebaseHelper.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Firebase;
+using Firebase.Database;
+using Firebase.Database.Query;
+using Microsoft.Maui.Storage;
+
+
+namespace BMICalculator
+{
+    internal class FirebaseHelper
+    {
+        FirebaseClient firebase = new FirebaseClient("https://bmicalculator-e0078-default-rtdb.asia-southeast1.firebasedatabase.app/");
+
+
+        public async Task AddRecord(string dt, double w, double br, string bs)
+        {
+            await firebase
+            .Child("BMIRecords")
+            .PostAsync(new BMIRecord() { DateRecorded = dt, Weight = w, BMIResult = br, BMIStatus = bs });
+        }
+
+        public async Task<List<BMIRecord>> GetAllBMIRecord()
+        {
+            return (await firebase
+            .Child("BMIRecords")
+            .OnceAsync<BMIRecord>()).Select(item => new BMIRecord
+            {
+                DateRecorded = item.Object.DateRecorded,
+                Weight = item.Object.Weight,
+                BMIResult = item.Object.BMIResult,
+                BMIStatus = item.Object.BMIStatus
+            }).ToList();
+        }
+    }
+}
